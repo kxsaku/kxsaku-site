8bc7293ade6a3aecf33f7f35642b15f04f41acc2
kxsaku <therealboltgamez@gmail.com>
2026-01-08 19:49:48 -0800

    adding reactbits home bg - v3

--- sns-site/src/App.jsx
+++ sns-site/src/App.jsx
@@ -3,27 +3,26 @@ import "./beams.css";
 
 export default function App() {
   return (
-    <div style={{ minHeight: "100vh", position: "relative" }}>
-      {/* Fullscreen background */}
-      <div
-        style={{
-          position: "fixed",
-          inset: 0,
-          zIndex: -1,
-          pointerEvents: "none",
-        }}
-      >
-        <Beams
-          beamWidth={2}
-          beamHeight={15}
-          beamNumber={12}
-          lightColor="#8565e2"
-          speed={2}
-          noiseIntensity={1.75}
-          scale={0.2}
-          rotation={0}
-        />
-      </div>
+    <div
+      style={{
+        position: "fixed",
+        inset: 0,
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+        background: "#000",   // keep it dark
+      }}
+    >
+      <Beams
+        beamWidth={2}
+        beamHeight={15}
+        beamNumber={12}
+        lightColor="#8565e2"
+        speed={2}
+        noiseIntensity={1.75}
+        scale={0.2}
+        rotation={0}
+      />
     </div>
   );
 }
