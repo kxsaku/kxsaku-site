68675b7c4fe7d198a72575fb5004f56b6d3568de
kxsaku <120144810+kxsaku@users.noreply.github.com>
2026-01-09 16:27:19 -0800

    updating home page with new title screen. - v35

--- react-headline/src/main.jsx
+++ react-headline/src/main.jsx
@@ -3,17 +3,10 @@ import ReactDOM from 'react-dom/client'
 import RotatingText from './RotatingText'
 import './RotatingText.css'
 
-ReactDOM.createRoot(document.getElementById('rt-react-root')).render(
-  <React.StrictMode>
-    <RotatingText
-      texts={['Wi-Fi', 'Networks', 'Security', 'Stability']}
-      staggerFrom="last"
-      initial={{ y: '100%' }}
-      animate={{ y: 0 }}
-      exit={{ y: '-120%' }}
-      staggerDuration={0.025}
-      transition={{ type: 'spring', damping: 30, stiffness: 400 }}
-      rotationInterval={2000}
-    />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RotatingText
+    texts={['Reliable', 'Secure', 'Scalable', 'Managed']}
+    rotationInterval={2000}
+    staggerFrom="last"
+  />
 )
